94a4835a5acd0a7fa63eac4f07a15fedf5c3e6e4
Frank S. Bara <fbara1@yahoo.com>
2019-07-27 09:52:55 -0500

    Initial Commit

---
+++ Ratings & Reviews/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Ratings & Reviews
+//
+//  Created by Frank Bara on 7/27/19.
+//  Copyright © 2019 BaraLabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
