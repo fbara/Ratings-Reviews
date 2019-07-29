bb18323a2a1a5f28f52919ea8a1a54efed372ede
Frank S. Bara <fbara1@yahoo.com>
2019-07-29 12:27:08 -0500

    Added stars and rating bars.

--- Ratings & Reviews/ContentView.swift
+++ Ratings & Reviews/ContentView.swift
@@ -10,7 +10,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+    
+        HStack(alignment: .top) {
+            NumberRatingView()
+            .padding(.trailing)
+            
+            StarView()
+            .padding(.trailing)
+            
+            VStack {
+                RatingLineView()
+                    .padding(.bottom, 4)
+                RatingLineView()
+                    .padding(.bottom, 4)
+                RatingLineView()
+                    .padding(.bottom, 4)
+                RatingLineView()
+                    .padding(.bottom, 4)
+                RatingLineView()
+                    .padding(.bottom, 4)
+            }
+                .padding(.top, 6)
+        }
     }
 }
 
@@ -21,3 +42,64 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 #endif
+
+// TODO: Make a new .swift file!
+struct NumberRatingView: View {
+    var body: some View {
+        VStack {
+            Text("4.8")
+                .font(Font.system(size: 72))
+                .fontWeight(.heavy)
+                
+                Text("out of 5")
+                    .font(.title)
+                    .fontWeight(.heavy)
+            }
+        }
+    }
+    
+// TODO: Make a new .swift file!
+    struct StarView: View {
+        var body: some View {
+            VStack(alignment: .trailing) {
+                Text("⭐️⭐️⭐️⭐️⭐️")
+                Text("⭐️⭐️⭐️⭐️")
+                Text("⭐️⭐️⭐️")
+                Text("⭐️⭐️")
+                Text("⭐️")
+            }
+        }
+    }
+
+// TODO: Make a new .swift file!
+    struct BottomLineView: View {
+        var body: some View {
+            Path { path in
+                path.move(to: CGPoint(x: 0, y: 0))
+                path.addRect(CGRect(x: 0, y: 0, width: 136, height: 10))
+            }
+            .fill(Color.secondary)
+            .cornerRadius(12)
+    }
+}
+
+// TODO: Make a new .swift file!
+    struct RatingLineView: View {
+        var rating = [8, 12, 27, 42, 72, 87, 112, 120, 133]
+        
+        var body: some View {
+            ZStack {
+                BottomLineView()
+                
+                Path { path in
+                    path.move(to: CGPoint(x: 0, y: 0))
+                    path.addRect(CGRect(x: 0, y: 0, width: rating.randomElement()!, height: 10))
+                }
+                    .cornerRadius(12)
+                    .frame(width: 136.0)
+            }
+            .frame(height: 10.0)
+    }
+}
+
+
